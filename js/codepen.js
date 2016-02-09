11eebbe2a1c58253eec0c032d8f1a0b564deae58
Anand Toshniwal <anand.toshniwal@qed42.com>
2016-02-09 18:38:00 +0530

    INT-181: PI to the nth decimal digit

--- js/codepen.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-  if ( $(window).width() > 1024) {
-  $("#zen-resources").css({'height':(($("#zen-participation").height())+'px')});
-  $(".sidebar").css({'height':(($(".supporting").height()-$("footer").height()+$(".height").height()-30  ) +'px')});
-
-}
-});
-
